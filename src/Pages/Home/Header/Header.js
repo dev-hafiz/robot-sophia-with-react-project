c2ba5a0920329978043b81b10cf384b7442a23bf
okshi <johnstarhafiz@gmail.com>
2021-10-13 13:09:46 +0600

    I impliment react bootstrap nav for tihs project"

--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logoRobot from '../../../images/logo/logo.png';
 import './Header.css';
 
@@ -8,23 +8,31 @@ const Header = () => {
      
      return (
           <div className="top-nav">
-             <Navbar bg="light" variant="light">
+             <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
                <Container>
-               <NavLink  to="/home" className="d-flex align-items-center logo-top"> <img
+               <Navbar.Brand href="#home">
+               <img
                     src={logoRobot}
-                    width="50"
-                    height="50"
-                    className="d-inline-block align-top me-3"
+                    width="240"
+                    height="60"
+                    className="d-inline-block align-top"
                     alt=""
-                    />Robot Sophia</NavLink>
-               <Nav className="ms-auto">
-                    <NavLink className="me-3 link-style text-dark fw-normal" to="/about">ABOUT</NavLink>
-                    <NavLink className="me-3 link-style text-dark" to="/robots">ROBOTS</NavLink>
-                    <NavLink className="me-3 link-style text-dark" to="/hansonai">HANSON AI</NavLink>
-                    <NavLink className="me-3 link-style text-dark" to="/resources">RESOURCES</NavLink>
-                    <NavLink className="me-3 link-style text-dark" to="/contact">CONTACT</NavLink>
-                   
-               </Nav>
+                    />
+               </Navbar.Brand>
+               <Navbar.Toggle />
+               <Navbar.Collapse className="justify-content-end">
+
+                    <Nav.Link as={Link} to="/about">ABOUT</Nav.Link>
+                    <Nav.Link as={Link} to="/robots">ROBOTS</Nav.Link>
+                    <Nav.Link as={Link} to="/hansonai">HANSON AI</Nav.Link>
+                    <Nav.Link as={Link} to="/resources">RESOURCES</Nav.Link>
+                    <Nav.Link as={Link} to="/contact">CONTACT</Nav.Link>
+
+                    <Navbar.Text>
+                    Signed in as: <a href="#login">Mark Otto</a>
+                    </Navbar.Text>
+               </Navbar.Collapse>
+               
                </Container>
                </Navbar>
           </div>
